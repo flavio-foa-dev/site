27c3bcc0492dfb8c443a4555d3cfc84c4a5774ad
Flavio Andrade <foa12381@gmail.com>
2022-07-30 11:28:34 -0300

    feat/gallery carousel

--- src/javaScript/toggle.js
+++ src/javaScript/toggle.js
@@ -1,4 +1,34 @@
 const btnMobile = document.getElementById('btn-mobile');
+const btn = document.querySelectorAll('.control')
+const images = document.querySelectorAll('.image')
+let focus = 0;
+const gallery = images.length
+
+btn.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const left = e.target.classList.contains("left")
+    if (left){
+      focus -= 1;
+    }else{
+      focus += 1;
+    }
+    if (focus >= gallery) {
+      focus = 0;
+    }
+    if (focus < 0) {
+      focus = gallery -1;
+    }
+
+    images.forEach(image => image.classList.remove("atual"));
+    images[focus].scrollIntoView({
+      inline:'center',
+      behavior:"smooth",
+  })
+    images[focus].classList.add("atual");
+  })
+})
+
+
 
 function toggleMobile(){
 const nav = document.getElementById('nav');
@@ -7,3 +37,10 @@ nav.classList.toggle('active');
 }
 
 btnMobile.addEventListener('click',toggleMobile);
+
+
+
+
+
+
+
